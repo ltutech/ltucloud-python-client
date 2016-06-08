8b5f2bd7e751695ea05e4d7a0be323248cb330af
Yannick Allusse <yallusse@ltutech.com>
2016-06-08 16:23:49 +0200

    add first LTU Cloud client class implementation
    This client allows to add/delete visuals
    and perform image searchs.

--- README.md
+++ README.md
@@ -1,2 +1,89 @@
-# ltucloud-python-client
-LTU Cloud Python client
+==================================================
+====== LTU Cloud - Python Client ======
+==================================================
+
+==============================================
+ABOUT THIS CLIENT
+==============================================
+
+Jastec France is a company that provides image recognition as a service.
+This Python module is a client that allows you to access the HTTP API to
+perform image recognition tasks. For more information on image recognition,
+please visit http://www.jastec.fr.
+
+
+Note that this client cannot be used without a valid LTU Cloud account.
+Please contact our sales department to get your account today
+(sales@jastec.fr) or create an evaluation account Online
+(http://www.jastec.fr/get_started/).
+
+==============================================
+INSTALL
+==============================================
+
+The client comes in the form of a multiplatform python package. Although the
+package has been tested on linux platforms only, it should run fine under
+Windows.
+
+Package can be installed along with dependencies by running:
+
+  python setup.py install
+
+Note that python 2.6.+ or later is required. Your python version can be
+obtained by running:
+
+  python --version
+
+==============================================
+BASIC USAGE
+==============================================
+
+Adding a visual with images and metadata:
+
+  from client import CloudClient
+  client = CloudClient(MY_LOGIN, MY_PASSWORD)
+  image_files = [image_path1]
+  metadata = {'data': 'None'}
+  visual_id = client.add_visual("Visual Title",
+                                "Visual Name",
+                                MY_PROJECT_ID,
+                                images=image_files,
+                                metadata=metadata)
+
+Delete a visual:
+
+  from client import CloudClient
+  client = CloudClient(MY_LOGIN, MY_PASSWORD)
+  visual_id = ...
+  client.delete_visual(visual_id)
+
+
+Perform an image search:
+
+  from client import CloudClient
+  client = CloudClient(MY_LOGIN, MY_PASSWORD)
+  result = client.search_image(image_path)
+  print(result)
+
+==============================================
+ADVANCED USAGE
+==============================================
+
+For advanced usage, please consult the docstrings for each function.
+
+==============================================
+LICENSE
+==============================================
+
+This software is licensed under the terms of the Apache License 2.0. In
+particular, you are free to distribute it, modify it and to distribute modified
+versions as long as you include the attached NOTICE file with your software.
+Read the attached LICENSE file for more information.
+
+===============================================
+CREDITS & USAGE:
+===============================================
+
+Feel free to use any of the resources provided in this client.
+
+EMAIL: support@jastec.fr
---
+++ ltu/cloud/client.py
@@ -0,0 +1,164 @@
+import logging
+import os
+import requests
+from urllib.parse import urlparse
+
+logger = logging.getLogger(__name__)
+
+class CloudClient(object):
+  """This class contains basic methods for accessing the API.
+  """
+  DEFAULT_QUERY_URL = "https://cloud.ltutech.com/api/v1/"
+
+  def __init__(self, login, password, server_url=DEFAULT_QUERY_URL):
+    """Constructor
+    Args:
+      application_key:  authentication key provided by the application.
+      server_url:       complete http url to the OnDemand server.
+    """
+    self.auth = (login, password)
+    self.server_url = server_url
+
+
+  def get_url(self, service):
+    """Combine a service name and the server url to produce the service url.
+    """
+    return requests.compat.urljoin(self.server_url, service)
+
+
+  def get_data(self, params={}):
+    """Return appropriate HTTP POST parameters
+
+    Args:
+      params: a dictionary with service-specific parameters
+    Returns:
+      filtered_params to be passed to requests.
+    """
+    data = []
+    for key, val in params.items():
+      if val is not None:
+        if isinstance(val, (list, tuple, set)):
+          for v in val:
+            data.append((key, v));
+        else:
+          data.append((key, val))
+    return data
+
+
+  def _post(self, service, params={}, files=None):
+    """Open corresponding API service with appropriate parameters.
+
+    Args:
+      service: service name
+      params: a dictionary of arguments to be passed to the service
+      files: dict of file object to transfer
+    Returns:
+      The json response content.
+    """
+    data    = self.get_data(params)
+    url     = self.get_url(service)
+    logger.debug("Posting to '%s'" % url)
+    request = requests.post(url, auth=self.auth, data=data, files=files)
+    status_code = request.status_code
+    answer = request.json()
+    if status_code not in [200, 201]:
+        raise Exception("Error while posting to %s (code: %d, msg: %s)" % (url, status_code, answer['errors']))
+    return answer
+
+
+  def _get(self, service, params={}):
+    """Open corresponding API service with appropriate parameters.
+
+    Args:
+      service: service name
+      params: a dictionary of arguments to be passed to the service
+    Returns:
+      The json response content.
+    """
+    data    = self.get_data(params)
+    url     = self.get_url(service)
+    logger.debug("Getting from '%s'" % url)
+    request = requests.get(url, auth=self.auth, data=data)
+    status_code = request.status_code
+    answer = request.json()
+    if status_code not in [200, 201]:
+        raise Exception("Error while getting from %s (code: %d, msg: %s)" % (url, status_code, answer['errors']))
+    return answer
+
+
+  def _delete(self, service):
+    """Open corresponding API service with appropriate parameters.
+
+    Args:
+      service: service name
+    """
+    url     = self.get_url(service)
+    logger.debug("Deleting '%s'" % url)
+    request = requests.delete(url, auth=self.auth)
+    status_code = request.status_code
+    if status_code not in [204]:
+        raise Exception("Error while deleting %s (code: %d)" % (url, status_code))
+
+
+  def search_image(self, image):
+    """Image retrieval based on a image stored on disk
+
+    Args:
+      image: path to image file.
+    """
+    logger.info("Search image %s" % image)
+    with open(image, 'rb') as img:
+      return self._post("queries", files={"image": img})
+
+
+  def add_visual(self, title, name, project_id, images=[], metadata={}):
+    """Create a new visual with some images
+    Returns:
+        The visual ID
+    """
+    logger.info("Adding visual: %s / %s" % (title, name))
+    # create the visual
+    params = {'title': title,
+              'name': name
+             }
+    result = self._post("projects/%d/visuals/" % project_id, params=params)
+    # TODO: manage existing visual
+    # collect visual id
+    visual_id = result['id']
+    # add images
+    self.add_images_to_visual(visual_id, images)
+    # add meta data
+    self.add_metadata_to_visual(visual_id, metadata)
+    return visual_id
+
+
+  def add_images_to_visual(self, visual_id, images=[]):
+    """Add image fomes to an existing visual_id
+
+    Args:
+      visual_id: LTU Cloud visual id
+      images: list of image files to add to the visual
+    """
+    for image in images:
+      logger.info("Adding image %s to visual %d" % (image, visual_id))
+      with open(image, 'rb') as img:
+        result = self._post("projects/visuals/%d/images/" % visual_id, files={'image':img})
+
+
+  def add_metadata_to_visual(self, visual_id, metadata={}):
+    """Add given metadata to Visual
+
+    Args:
+      visual_id: LTU Cloud visual id
+      metadata: dict of metadata to add to the visual
+    """
+    for key, value in metadata.items():
+      logger.info("Adding metadata (%s:%s) to visual %d" % (key, value, visual_id))
+      result = self._post("projects/visuals/%d/metadata/" % visual_id,
+                          params={'key':key, 'value':value})
+
+
+  def delete_visual(self, visual_id):
+    """Remove a visual from the database"""
+    logger.info("Deleting visual %d" % visual_id)
+    self._delete("projects/visuals/%d/" % visual_id)
