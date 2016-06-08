feb89d5205dcf719445cbbf677c166d8c6709ae7
Yannick Allusse <yallusse@ltutech.com>
2016-06-08 16:22:31 +0200

    add base project files
    Add license, setup.py, modules, etc...

---
+++ ltu/__init__.py
@@ -0,0 +1,7 @@
+# Declare 'ltu' namespace
+# @see http://www.python.org/dev/peps/pep-0382/
+try:
+  __import__('pkg_resources').declare_namespace(__name__)
+except ImportError:
+  from pkgutil import extend_path
+  __path__ = extend_path(__path__, __name__)
