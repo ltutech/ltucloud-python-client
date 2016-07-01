fc61eee5641a8f7ba3a8a5392f0fe5a06d9cc3bf
Jonathan Piron <jonathanpiron@gmail.com>
2016-07-01 11:27:15 +0200

    Add query related factories

--- ltu/cloud/factories.py
+++ ltu/cloud/factories.py
@@ -65,6 +65,18 @@ class ImageFactory(ResourceCommonFactory):
     source = factory.LazyAttribute(lambda _: faker.word())
 
 
+class MatchedImageFactory(ImageFactory):
+    """Create LTU Cloud MatchedImage objects."""
+
+    class Meta:
+        """Setup the factory model."""
+
+        model = models.MatchedImage
+
+    score = factory.LazyAttribute(lambda _: faker.pyfloat(left_digits=3, right_digits=2,
+                                                          positive=True))
+
+
 class MetadataFactory(ResourceCommonFactory):
     """Create LTU Cloud metadata objects."""
 
@@ -115,3 +127,65 @@ class VisualFactory(ResourceCommonFactory):
         if self.nb_metadatas:
             MetadataFactory.create_batch(size=self.nb_metadatas)
         return metadatas
+
+
+class MatchedVisualFactory(VisualFactory):
+    """Create LTU Cloud MatchedVisual objects."""
+
+    class Meta:
+        """Setup the factory model."""
+
+        model = models.MatchedVisual
+        # remove Visual only attributes
+        exclude = ("cover_id", "_media", "images")
+
+    match_count = factory.LazyAttribute(lambda _: faker.pyint())
+
+    @factory.lazy_attribute
+    def matched_images(self):
+        """Create matched_images."""
+        matched_images = []
+        if self.nb_images:
+            matched_images = MatchedImageFactory.create_batch(size=self.nb_images)
+        return matched_images
+
+
+class MatchFactory(VisualFactory):
+    """Create LTU Cloud Match objects."""
+
+    class Meta:
+        """Setup the factory model."""
+
+        model = models.Match
+
+    id = factory.Sequence(lambda n: n)
+    nb_matched_images = factory.LazyAttribute(lambda _: faker.pyint())
+    score = score = factory.LazyAttribute(lambda _: faker.pyfloat(left_digits=3, right_digits=2,
+                                                                  positive=True))
+    _links = factory.SubFactory(LinksFactory)
+    matched_visual = factory.SubFactory(MatchedVisualFactory)
+
+
+class QueryFactory(ResourceCommonFactory):
+    """Create LTU Cloud Query objects."""
+
+    class Meta:
+        """Setup the factory model."""
+
+        model = models.Query
+        exclude = ("nb_matches", )
+
+    _links = factory.SubFactory(LinksFactory)
+    _media = factory.SubFactory(MediaFactory)
+    nb_matches = 1  # number of matches to associate to this visual
+    source = factory.LazyAttribute(lambda _: faker.word())
+    source_description = factory.LazyAttribute(lambda _: faker.sentence(nb_words=6,
+                                                                        variable_nb_words=True))
+
+    @factory.lazy_attribute
+    def matches(self):
+        """Create matches."""
+        matches = []
+        if self.nb_matches:
+            matches = MatchFactory.create_batch(size=self.nb_matches)
+        return matches
