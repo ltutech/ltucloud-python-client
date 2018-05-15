97dc622322f6c734b6a8f6dc2c59550f6adb3976
pauline-jastec <pdufour@jastec.fr>
2018-05-15 11:44:06 +0200

    Delete __init__.py

--- ltu/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Declare 'ltu' namespace.
-
-@see http://www.python.org/dev/peps/pep-0382/
-"""
-try:
-    __import__('pkg_resources').declare_namespace(__name__)
-except ImportError:
-    from pkgutil import extend_path
-    __path__ = extend_path(__path__, __name__)
